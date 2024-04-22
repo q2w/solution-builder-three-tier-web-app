7a439a66ce8f9b642865786cd07b5b72a59b17b1
Ayush Jain <ayushmjain@google.com>
2024-04-22 06:16:09 +0000

    use remote modules

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,12 @@
 module "three-tier-app-vpc-network" {
-    source = "./modules/terraform-google-solution-builder-vpc-network"
+    source = "github.com/ayushmjain/terraform-google-solution-builder-vpc-network?ref=v1.0.0"
     project_id = var.project_id
     region = var.region
     network_name = var.three-tier-app-vpc-network-network_name
 }
 
 module "three-tier-app-cache" {
-    source = "./modules/terraform-google-solution-builder-redis"
+    source = "github.com/ayushmjain/terraform-google-solution-builder-redis?ref=v1.0.0"
     project_id = var.project_id
     region = var.region
     redis_instance_name = var.three-tier-app-cache-redis_instance_name
@@ -16,7 +16,7 @@ module "three-tier-app-cache" {
 }
 
 module "three-tier-app-database" {
-    source = "./modules/terraform-google-solution-builder-cloud-sql"
+    source = "github.com/ayushmjain/terraform-google-solution-builder-cloud-sql?ref=v1.0.0"
     project_id = var.project_id
     region = var.region
     database_name = var.three-tier-app-database-database_name
@@ -28,7 +28,7 @@ module "three-tier-app-database" {
 }
 
 module "three-tier-app-backend" {
-    source = "./modules/terraform-google-solution-builder-cloud-run"
+    source = "github.com/ayushmjain/terraform-google-solution-builder-cloud-run?ref=v1.0.1"
     project_id = var.project_id
     region = var.region
     cloud_run_service_name = var.three-tier-app-backend-cloud_run_service_name
@@ -42,7 +42,7 @@ module "three-tier-app-backend" {
 }
 
 module "three-tier-app-frontend" {
-    source = "./modules/terraform-google-solution-builder-cloud-run"
+    source = "github.com/ayushmjain/terraform-google-solution-builder-cloud-run?ref=v1.0.1"
     project_id = var.project_id
     region = var.region
     cloud_run_service_name = var.three-tier-app-frontend-cloud_run_service_name
