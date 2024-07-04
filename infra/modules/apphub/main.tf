5a04b5ed2266f943a469e2565f16b5adf5bbca6b
rrmsha <99204705+rrmsha@users.noreply.github.com>
2024-07-04 12:41:19 +0530

    created apphub application and defined its variables

---
+++ infra/modules/apphub/main.tf
@@ -0,0 +1,30 @@
+#Creating the application
+resource "google_apphub_application" "apphub_application" {
+  location = var.app_location
+  application_id = var.application_name
+  display_name = "AppHub Application"
+  scope {
+    type = var.scope_type
+  }
+  description = "Application"
+  attributes {
+    environment {
+      type = var.environment_type
+        }
+        criticality {  
+      type = var.criticality_type
+        }
+        business_owners {
+          display_name =  var.owner_name
+          email        =  var.owner_email
+        }
+        developer_owners {
+          display_name =  var.owner_name
+          email        =  var.owner_email
+        }
+        operator_owners {
+          display_name =  var.owner_name
+          email        =  var.owner_email
+        }
+  }
+}
---
+++ infra/modules/apphub/variables.tf
@@ -0,0 +1,27 @@
+variable "app_location"{
+    type = string
+}
+
+variable "application_name"{
+    type = string
+}
+
+variable "scope_type"{
+    type = string
+}
+
+variable "environment_type"{
+    type = string
+}
+
+variable "criticality_type"{
+    type = string
+}
+
+variable "owner_name"{
+    type = string
+}
+
+variable "owner_email"{
+    type = string
+}
